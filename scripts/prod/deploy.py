30a6b8af2d87fd02172dc4a78fd893c04dc7b87c
Max <mopitz199@gmail.com>
2021-01-23 20:47:30 -0300

    .

--- scripts/prod/deploy.py
+++ scripts/prod/deploy.py
@@ -14,11 +14,11 @@ source ./.colors
 # Build and bring up new containers
 git pull
 docker-compose build --pull api
-docker-compose -p $IDLE_COLOR up -d --scale api=$REPLICAS --force-recreate api
+docker-compose -f docker-compose-prod.yml -p $IDLE_COLOR up -d --scale api=1 --force-recreate api
 
 # Bring down old containers once new ones are working
-while [[ $(curl -s --location --request GET 'http://localhost/deployment_color' | tr -d \") != $IDLE_COLOR ]]; do :; done
-docker-compose -p $DEPLOYED_COLOR down
+while [[ $(curl -s --location --request GET 'http://localhost/deployment_color' | tr -d \") == $IDLE_COLOR ]]; do :; done
+docker-compose -f docker-compose-prod.yml -p $DEPLOYED_COLOR down
 
 # Update the .colors config
 echo -e "export DEPLOYED_COLOR="${IDLE_COLOR} > .colors
