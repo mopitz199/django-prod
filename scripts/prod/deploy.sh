932fc66da730b06e1401e071dc361a35201fa90e
Max <mopitz199@gmail.com>
2021-01-23 23:52:36 -0300

    .

--- scripts/prod/deploy.sh
+++ scripts/prod/deploy.sh
@@ -1,7 +1,7 @@
 # Check if docker network is present (if not, this deployment is being done the first time)
 docker network create main;
 DOCKER_NETWORK_ID=$(docker network inspect main -f '{{ .Id }}')
-if [[ "${DOCKER_NETWORK_ID}" != "" ]]; then 
+if [[ "${DOCKER_NETWORK_ID}" == "" ]]; then 
     docker-compose -f docker-compose-prod.yml up -d traefik;
     touch ./.colors;
     echo -e "export DEPLOYED_COLOR=blue" > .colors;
@@ -14,7 +14,7 @@ source ./.colors
 # Build and bring up new containers
 # git pull
 # docker-compose build --pull api
-docker-compose -f docker-compose-prod.yml -p $IDLE_COLOR up -d -e DEPLOYMENT_COLOR=${IDLE_COLOR}  --scale api=1 --force-recreate api
+docker-compose -f docker-compose-prod.yml up -p $IDLE_COLOR  -d -e DEPLOYMENT_COLOR=$IDLE_COLOR  --scale api=1 --force-recreate api
 
 # Bring down old containers once new ones are working
 while [[ $(curl -s --location --request GET 'http://localhost/deployment_color' | tr -d \") != $IDLE_COLOR ]]; do :; done
